a1254679f42bd6967ad806e2cba8b893b271af15
zzm <sea3@qq.com>
2020-11-09 22:06:02 +0800

    1

--- pinry/core/mayun.py
+++ pinry/core/mayun.py
@@ -8,38 +8,39 @@ from .models import Token
 
 header_dict = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Trident/7.0; rv:11.0) like Gecko'}
-TOKEN = 'BC93B122D2BD4A5B9001EC148BF46F5A'
+TOKEN = 'd5dc3b7296a0da149eefa9b4ad9e6492'
 
-api = "http://to.banma1024.com"
+api = "http://gateway.cszbdp.com/api2/"
 
 def login():
-    # url = 'http://xiangjiuer.cn/sms/api/login?username=API用户名&password=密码' % ('zzm88','1988104')    
-    url = 'http://yima.hbtfcc.com:2019/WebAPI/login?uname=%s&upwd=%s' % ('zzm88','123456a')    
+    url = '	http://gateway.cszbdp.com/api2/login?member_no=%s&pwd=%s' % ('zzm88','123456a')    
     response = requests.get(url=url, headers=header_dict).content.decode('utf-8').encode('gb2312')
     response = response.strip('"') 
     global TOKEN 
-    if response.split('|')[0] == '1': #登陆成功
-        TOKEN = response.split('|')[1]
+    if response.split(':')[0] == '1': #登陆成功
+        TOKEN = response.split(':')[1]
     else:
         pass
-    
+
+#测试链接    
+# http://localhost:8000/getphone_new/188
 
 # Create your tests here.
 def getphone(ITEMID,PHONENUM=''):
     if PHONENUM != '':
         PHONENUM = '&phone=%s' % (PHONENUM)
-    url = 'http://yima.hbtfcc.com:2019/WebAPI/GetPhone?ukey=%s&bid=%s' % (TOKEN,ITEMID) + PHONENUM
+    url = 'http://gateway.cszbdp.com/api2/getCardNo?token=%s&project_id=%s' % (TOKEN,ITEMID) + PHONENUM
     # response = requests.get(url=url, headers=header_dict).content.decode('utf-8').encode('gb2312')
     response = requests.get(url=url, headers=header_dict).content
     response = response.strip('"') 
-    if response.split('|')[1] == "身份验证失败，请重新登录" :#TOKEN错误，重新登录
+    if response.split(':')[1] == "token参数缺失" :#TOKEN错误，重新登录
        login()#登录
-    if response.split('|')[0] == "0" :
+    if response.split(':')[0] == "0" :
         # login()#登录
-        return response.split('|')[1]
-    elif response.split('|')[0] == "1" : #取号成功
+        return response.split(':')[1] #失败则返回错误信息
+    elif response.split(':')[0] == "1" : #取号成功
       
-        phone_number = response.split('|')[1]
+        phone_number = response.split(':')[1]
         return phone_number
 
 def getphone_token(ITEMID,PHONENUM=''):
@@ -62,18 +63,18 @@ def getsms(PHONENUM,ITEMID):
     WAIT = 60 # 接受短信时长60s
     
    
-    url = 'http://yima.hbtfcc.com:2019/WebAPI/GetMessage?ukey=%s&bid=%s&phone=%s' % (TOKEN,ITEMID,PHONENUM)
+    url = 'http://gateway.cszbdp.com/api2/getSmsContent?token=%s&project_id=%s&card_no=%s&sms_seq=1' % (TOKEN,ITEMID,PHONENUM)
       
     response = requests.get(url=url, headers=header_dict).content
     response = response.strip('"') 
-    if response.split('|')[1] == "身份验证失败，请重新登录" :#TOKEN错误，重新登录
+    if response.split(':')[1] == "身份验证失败，请重新登录" :#TOKEN错误，重新登录
        login()#登录
     TIME1 = time.time()
     TIME2 = time.time()
     ROUND = 1
 
 
-    while (TIME2-TIME1) < WAIT and response.split('|')[0] == '0': # 60秒内且未成功
+    while (TIME2-TIME1) < WAIT and response.split(':')[0] == '0': # 60秒内且未成功
 
         
         time.sleep(5)
@@ -84,12 +85,12 @@ def getsms(PHONENUM,ITEMID):
         print "try"+str(ROUND)
         
     ROUND = str(ROUND)
-    if response.split('|')[0] == '1':# 若成功
+    if response.split(':')[0] == '1':# 若成功
         
        
         TIME = str(round(TIME2-TIME1, 1))
 
-        d = {"time":TIME,"round":ROUND,"msg":response.split('|')[1]}
+        d = {"time":TIME,"round":ROUND,"msg":response.split(':')[1]}
         
         return d
     else:
@@ -99,17 +100,19 @@ def getsms(PHONENUM,ITEMID):
         d = {"error":response,"round":ROUND}
         return d
 
+
+# http://localhost:8000/release_new
 def releasephone(ITEMID,PHONENUM):
 
-    url = 'http://yima.hbtfcc.com:2019/WebAPI/CancelRecv?&ukey=%s&bid=%s&phone=%s' % (TOKEN,ITEMID,PHONENUM)
+    url = 'http://gateway.cszbdp.com/api2/releaseTimeoutCardNo?token=%s&project_id=%s&card_no=%s' % (TOKEN,ITEMID,PHONENUM)
     response = requests.get(url=url, headers=header_dict).content
     response = response.strip('"') 
-    if  response.split('|')[0] == "1" : #成功释放
+    if  response.split(':')[0] == "1" : #成功释放
         # MOBILE1 = response.split('|')[1]
           
         response = u"成功释放"
     else:
-        response = u'联系管理员，错误:'+  response.split('|')[1]
+        response = u'联系管理员，错误:'+  response.split(':')[1]
        
     return response
 # def releasephone(ITEMID,PHONENUM):
